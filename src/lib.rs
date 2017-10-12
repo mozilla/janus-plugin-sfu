bcd57b4e2869e9da4e21a6f3ceaf9239b1ef0da5
Marshall Quander <marshall@quander.me>
2017-10-12 13:23:11 -0700

    Better naming consistency

--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,8 @@ use serde_json::Value as JsonValue;
 use serde_json::Result as JsonResult;
 use janus::{JanssonValue, RawJanssonValue,
             LogLevel, Plugin, PluginCallbacks, PluginMetadata,
-            PluginResultInfo, PluginResultType, PluginHandle};
-use janus::session::SessionHandle;
+            PluginResultInfo, PluginResultType, PluginSession};
+use janus::session::SessionWrapper;
 
 /// Inefficiently converts a Jansson JSON value to a serde JSON value.
 pub fn to_serde_json(input: JanssonValue) -> JsonValue {
@@ -49,22 +49,22 @@ bitflags! {
 
 #[derive(Debug)]
 pub struct Subscription {
-    pub conn: Weak<Connection>,
+    pub sess: Weak<Session>,
     pub kind: ContentKind
 }
 
 #[derive(Debug)]
-pub struct ConnectionState {
+pub struct SessionState {
     pub user_id: AtomicUserId
 }
 
-impl Default for ConnectionState {
-    fn default() -> ConnectionState {
+impl Default for SessionState {
+    fn default() -> SessionState {
         Self { user_id: AtomicUserId::empty() }
     }
 }
 
-pub type Connection = SessionHandle<ConnectionState>;
+pub type Session = SessionWrapper<SessionState>;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase", tag = "type")]
@@ -76,20 +76,20 @@ pub enum JsepKind {
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase", tag = "kind")]
 pub enum MessageKind {
-    Join { user_id: Option<UserId>, role: ConnectionRole },
+    Join { user_id: Option<UserId>, role: SessionRole },
     List,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase", tag = "kind")]
-pub enum ConnectionRole {
+pub enum SessionRole {
     Publisher,
     Subscriber { target_id: UserId }
 }
 
 #[derive(Debug)]
 pub struct RawMessage {
-    pub conn: Weak<Connection>,
+    pub sess: Weak<Session>,
     pub txn: *mut c_char,
     pub msg: Option<JanssonValue>,
     pub jsep: Option<JanssonValue>,
@@ -118,7 +118,7 @@ fn gateway_callbacks() -> &'static PluginCallbacks {
 
 #[derive(Debug)]
 struct State {
-    pub connections: RwLock<Vec<Box<Arc<Connection>>>>,
+    pub sessions: RwLock<Vec<Box<Arc<Session>>>>,
     pub subscriptions: RwLock<HashMap<UserId, Vec<Subscription>>>,
     pub message_channel: Mutex<Option<mpsc::SyncSender<RawMessage>>>,
     pub next_user_id: AtomicUserId,
@@ -126,7 +126,7 @@ struct State {
 
 lazy_static! {
     static ref STATE: State = State {
-        connections: RwLock::new(Vec::new()),
+        sessions: RwLock::new(Vec::new()),
         subscriptions: RwLock::new(HashMap::new()),
         message_channel: Mutex::new(None),
         next_user_id: AtomicUserId::first()
@@ -134,9 +134,8 @@ lazy_static! {
 }
 
 fn notify(myself: UserId, msg: JanssonValue) -> Result<(), Box<Error+Send+Sync>> {
-    let connections = STATE.connections.read().unwrap();
     let push_event = gateway_callbacks().push_event;
-    for other in connections.iter() {
+    for other in STATE.sessions.read().unwrap().iter() {
         if other.user_id.load(Ordering::Relaxed) != Some(myself) {
             janus::get_result(push_event(other.handle, &mut PLUGIN, ptr::null(), msg.ptr, ptr::null_mut()))?
         }
@@ -175,11 +174,11 @@ extern "C" fn destroy() {
     janus::log(LogLevel::Info, "Janus retproxy plugin destroyed!");
 }
 
-extern "C" fn create_session(handle: *mut PluginHandle, error: *mut c_int) {
-    match Connection::associate(handle, Default::default()) {
-        Ok(conn) => {
-            janus::log(LogLevel::Info, &format!("Initializing retproxy session {:?}...", conn));
-            (*STATE.connections.write().unwrap()).push(conn);
+extern "C" fn create_session(handle: *mut PluginSession, error: *mut c_int) {
+    match Session::associate(handle, Default::default()) {
+        Ok(sess) => {
+            janus::log(LogLevel::Info, &format!("Initializing retproxy session {:?}...", sess));
+            STATE.sessions.write().unwrap().push(sess);
         },
         Err(e) => {
             janus::log(LogLevel::Err, &format!("{}", e));
@@ -188,15 +187,15 @@ extern "C" fn create_session(handle: *mut PluginHandle, error: *mut c_int) {
     }
 }
 
-extern "C" fn destroy_session(handle: *mut PluginHandle, error: *mut c_int) {
-    match Connection::from_ptr(handle) {
-        Ok(conn) => {
-            janus::log(LogLevel::Info, &format!("Destroying retproxy session {:?}...", conn));
-            let user_id = conn.user_id.load(Ordering::Relaxed);
-            STATE.connections.write().unwrap().retain(|ref c| c.handle != handle);
+extern "C" fn destroy_session(handle: *mut PluginSession, error: *mut c_int) {
+    match Session::from_ptr(handle) {
+        Ok(sess) => {
+            janus::log(LogLevel::Info, &format!("Destroying retproxy session {:?}...", sess));
+            let user_id = sess.user_id.load(Ordering::Relaxed);
+            STATE.sessions.write().unwrap().retain(|ref s| s.handle != handle);
 
             if let Some(user_id) = user_id {
-                let user_exists = STATE.connections.read().unwrap().iter().any(|ref c| Some(user_id) == c.user_id.load(Ordering::Relaxed));
+                let user_exists = STATE.sessions.read().unwrap().iter().any(|ref s| Some(user_id) == s.user_id.load(Ordering::Relaxed));
                 if !user_exists {
                     remove_publication(&user_id);
                     let response = from_serde_json(json!({"event": "leave", "user_id": user_id}));
@@ -213,24 +212,24 @@ extern "C" fn destroy_session(handle: *mut PluginHandle, error: *mut c_int) {
     }
 }
 
-extern "C" fn query_session(_handle: *mut PluginHandle) -> *mut RawJanssonValue {
+extern "C" fn query_session(_handle: *mut PluginSession) -> *mut RawJanssonValue {
     ptr::null_mut()
 }
 
-extern "C" fn setup_media(_handle: *mut PluginHandle) {
+extern "C" fn setup_media(_handle: *mut PluginSession) {
     janus::log(LogLevel::Verb, "WebRTC media is now available.");
 }
 
-fn relay<T>(from: *mut PluginHandle, kind: ContentKind, send: T) -> Result<(), Box<Error+Send+Sync>> where T: Fn(&Connection) {
-    let conn = Connection::from_ptr(from)?;
-    if let Some(user_id) = conn.user_id.load(Ordering::Relaxed) {
+fn relay<T>(from: *mut PluginSession, kind: ContentKind, send: T) -> Result<(), Box<Error+Send+Sync>> where T: Fn(&Session) {
+    let sess = Session::from_ptr(from)?;
+    if let Some(user_id) = sess.user_id.load(Ordering::Relaxed) {
         janus::log(LogLevel::Dbg, &format!("Packet of kind {:?} received from {:?}.", kind, user_id));
         if let Some(subscriptions) = STATE.subscriptions.read().unwrap().get(&user_id) {
             for subscription in subscriptions {
-                if let Some(subscriber_conn) = subscription.conn.upgrade() {
+                if let Some(subscriber_sess) = subscription.sess.upgrade() {
                     if subscription.kind.contains(kind) {
-                        janus::log(LogLevel::Dbg, &format!("Forwarding packet from {:?} to {:?}.", user_id, **subscriber_conn));
-                        send(subscriber_conn.as_ref());
+                        janus::log(LogLevel::Dbg, &format!("Forwarding packet from {:?} to {:?}.", user_id, **subscriber_sess));
+                        send(subscriber_sess.as_ref());
                     }
                 }
             }
@@ -241,7 +240,7 @@ fn relay<T>(from: *mut PluginHandle, kind: ContentKind, send: T) -> Result<(), B
     }
 }
 
-extern "C" fn incoming_rtp(handle: *mut PluginHandle, video: c_int, buf: *mut c_char, len: c_int) {
+extern "C" fn incoming_rtp(handle: *mut PluginSession, video: c_int, buf: *mut c_char, len: c_int) {
     let content_kind = if video == 1 { ContentKind::VIDEO } else { ContentKind::AUDIO };
     let relay_rtp = gateway_callbacks().relay_rtp;
     if let Err(e) = relay(handle, content_kind, |other| { relay_rtp(other.handle, video, buf, len); }) {
@@ -249,7 +248,7 @@ extern "C" fn incoming_rtp(handle: *mut PluginHandle, video: c_int, buf: *mut c_
     }
 }
 
-extern "C" fn incoming_rtcp(handle: *mut PluginHandle, video: c_int, buf: *mut c_char, len: c_int) {
+extern "C" fn incoming_rtcp(handle: *mut PluginSession, video: c_int, buf: *mut c_char, len: c_int) {
     let content_kind = if video == 1 { ContentKind::VIDEO } else { ContentKind::AUDIO };
     let relay_rtcp = gateway_callbacks().relay_rtcp;
     if let Err(e) = relay(handle, content_kind, |other| { relay_rtcp(other.handle, video, buf, len); }) {
@@ -257,56 +256,56 @@ extern "C" fn incoming_rtcp(handle: *mut PluginHandle, video: c_int, buf: *mut c
     }
 }
 
-extern "C" fn incoming_data(handle: *mut PluginHandle, buf: *mut c_char, len: c_int) {
+extern "C" fn incoming_data(handle: *mut PluginSession, buf: *mut c_char, len: c_int) {
     let relay_data = gateway_callbacks().relay_data;
     if let Err(e) = relay(handle, ContentKind::DATA, |other| { relay_data(other.handle, buf, len); }) {
         janus::log(LogLevel::Err, &format!("Discarding data packet: {}", e))
     }
 }
 
-extern "C" fn slow_link(_handle: *mut PluginHandle, _uplink: c_int, _video: c_int) {
+extern "C" fn slow_link(_handle: *mut PluginSession, _uplink: c_int, _video: c_int) {
     janus::log(LogLevel::Verb, "Slow link message received!");
 }
 
-extern "C" fn hangup_media(_handle: *mut PluginHandle) {
+extern "C" fn hangup_media(_handle: *mut PluginSession) {
     janus::log(LogLevel::Verb, "Hanging up WebRTC media.");
 }
 
 fn get_user_list() -> HashSet<UserId> {
-    let connections = STATE.connections.read().unwrap();
-    connections.iter().filter_map(|c| c.user_id.load(Ordering::Relaxed)).collect()
+    let sessions = STATE.sessions.read().unwrap();
+    sessions.iter().filter_map(|c| c.user_id.load(Ordering::Relaxed)).collect()
 }
 
-fn add_subscription(conn: &Arc<Connection>, to: UserId, kind: ContentKind) {
+fn add_subscription(sess: &Arc<Session>, to: UserId, kind: ContentKind) {
     let mut subscriptions = STATE.subscriptions.write().unwrap();
     let subscribers = subscriptions.entry(to).or_insert_with(Vec::new);
-    subscribers.push(Subscription { conn: Arc::downgrade(conn), kind });
+    subscribers.push(Subscription { sess: Arc::downgrade(sess), kind });
 }
 
 fn remove_publication(publisher: &UserId) {
     STATE.subscriptions.write().unwrap().remove(publisher);
 }
 
-fn handle_join(conn: &Arc<Connection>, txn: *mut c_char, user_id: Option<UserId>, role: ConnectionRole) -> MessageProcessingResult {
+fn handle_join(sess: &Arc<Session>, txn: *mut c_char, user_id: Option<UserId>, role: SessionRole) -> MessageProcessingResult {
     let other_user_ids = get_user_list();
 
     match role {
-        ConnectionRole::Subscriber { target_id } => {
-            add_subscription(&conn, target_id, ContentKind::AUDIO | ContentKind::VIDEO);
+        SessionRole::Subscriber { target_id } => {
+            add_subscription(&sess, target_id, ContentKind::AUDIO | ContentKind::VIDEO);
         }
-        ConnectionRole::Publisher => {
+        SessionRole::Publisher => {
             for other_user_id in &other_user_ids {
-                add_subscription(&conn, *other_user_id, ContentKind::DATA);
+                add_subscription(&sess, *other_user_id, ContentKind::DATA);
             }
         },
     };
 
     match user_id {
-        Some(n) => Ok(conn.user_id.store(n, Ordering::Relaxed)),
+        Some(n) => Ok(sess.user_id.store(n, Ordering::Relaxed)),
         None => {
             let new_user_id = STATE.next_user_id.next(Ordering::Relaxed)
                 .expect("next_user_id is always a non-empty user ID.");
-            conn.user_id.store(new_user_id, Ordering::Relaxed);
+            sess.user_id.store(new_user_id, Ordering::Relaxed);
             let notification = from_serde_json(json!({ "event": "join_other", "user_id": new_user_id }));
             if let Err(e) = notify(new_user_id, notification) {
                 janus::log(LogLevel::Err, &format!("Error sending notification for user join: {:?}", e))
@@ -318,19 +317,19 @@ fn handle_join(conn: &Arc<Connection>, txn: *mut c_char, user_id: Option<UserId>
                 "user_id": new_user_id,
                 "user_ids": other_user_ids
             }));
-            janus::get_result(push_event(conn.handle, &mut PLUGIN, txn, response.ptr, ptr::null_mut()))
+            janus::get_result(push_event(sess.handle, &mut PLUGIN, txn, response.ptr, ptr::null_mut()))
         }
     }
 }
 
-fn handle_list(conn: &Arc<Connection>, txn: *mut c_char) -> MessageProcessingResult {
+fn handle_list(sess: &Arc<Session>, txn: *mut c_char) -> MessageProcessingResult {
     let user_list = get_user_list();
     let push_event = gateway_callbacks().push_event;
     let response = from_serde_json(json!({"user_ids": user_list}));
-    janus::get_result(push_event(conn.handle, &mut PLUGIN, txn, response.ptr, ptr::null_mut()))
+    janus::get_result(push_event(sess.handle, &mut PLUGIN, txn, response.ptr, ptr::null_mut()))
 }
 
-fn handle_offer(conn: &Arc<Connection>, txn: *mut c_char, sdp: String) -> MessageProcessingResult {
+fn handle_offer(sess: &Arc<Session>, txn: *mut c_char, sdp: String) -> MessageProcessingResult {
     let offer = janus::sdp::parse_sdp(CString::new(sdp)?)?;
     let answer = answer_sdp!(offer, janus::sdp::OfferAnswerParameters::Video, 0);
     let answer_str = janus::sdp::write_sdp(&answer);
@@ -340,31 +339,31 @@ fn handle_offer(conn: &Arc<Connection>, txn: *mut c_char, sdp: String) -> Messag
         "sdp": answer_str.to_str()?
     }));
     let push_event = gateway_callbacks().push_event;
-    janus::get_result(push_event(conn.handle, &mut PLUGIN, txn, answer_msg.ptr, answer_jsep.ptr))
+    janus::get_result(push_event(sess.handle, &mut PLUGIN, txn, answer_msg.ptr, answer_jsep.ptr))
 }
 
-fn push_error<T>(conn: &Connection, txn: *mut c_char, err: Box<T>) -> MessageProcessingResult where T: Error+?Sized {
+fn push_error<T>(sess: &Session, txn: *mut c_char, err: Box<T>) -> MessageProcessingResult where T: Error+?Sized {
     let response = from_serde_json(json!({ "error": format!("{}", err) }));
     let push_event = gateway_callbacks().push_event;
-    janus::get_result(push_event(conn.handle, &mut PLUGIN, txn, response.ptr, ptr::null_mut()))
+    janus::get_result(push_event(sess.handle, &mut PLUGIN, txn, response.ptr, ptr::null_mut()))
 }
 
-fn handle_message_async(RawMessage { jsep, msg, txn, conn }: RawMessage) -> MessageProcessingResult {
-    if let Some(ref conn) = conn.upgrade() {
+fn handle_message_async(RawMessage { jsep, msg, txn, sess }: RawMessage) -> MessageProcessingResult {
+    if let Some(ref sess) = sess.upgrade() {
         // if we have a JSEP, handle it independently of whether or not we have a message
         jsep.map_or(Ok(()), |x| {
             let result: JsonResult<JsepKind> = serde_json::from_str(&x.to_string(0));
             match result {
                 Ok(kind) => {
-                    janus::log(LogLevel::Info, &format!("Processing {:?} on connection {:?}.", kind, conn));
+                    janus::log(LogLevel::Info, &format!("Processing {:?} on connection {:?}.", kind, sess));
                     match kind {
-                        JsepKind::Offer { sdp } => handle_offer(&conn, txn, sdp),
+                        JsepKind::Offer { sdp } => handle_offer(&sess, txn, sdp),
                         JsepKind::Answer { .. } => {
-                            push_error(conn, txn, MessageProcessingError::from("JSEP answers not yet supported."))
+                            push_error(sess, txn, MessageProcessingError::from("JSEP answers not yet supported."))
                         }
                     }
                 },
-                Err(e) => push_error(conn, txn, Box::new(e))
+                Err(e) => push_error(sess, txn, Box::new(e))
             }
         })?;
         // if we have a message, handle that
@@ -372,13 +371,13 @@ fn handle_message_async(RawMessage { jsep, msg, txn, conn }: RawMessage) -> Mess
             let result: JsonResult<MessageKind> = serde_json::from_str(&x.to_string(0));
             match result {
                 Ok(kind) => {
-                    janus::log(LogLevel::Info, &format!("Processing {:?} on connection {:?}.", kind, conn));
+                    janus::log(LogLevel::Info, &format!("Processing {:?} on connection {:?}.", kind, sess));
                     match kind {
-                        MessageKind::List => handle_list(&conn, txn),
-                        MessageKind::Join { user_id, role } => handle_join(&conn, txn, user_id, role),
+                        MessageKind::List => handle_list(&sess, txn),
+                        MessageKind::Join { user_id, role } => handle_join(&sess, txn, user_id, role),
                     }
                 },
-                Err(e) => push_error(conn, txn, Box::new(e))
+                Err(e) => push_error(sess, txn, Box::new(e))
             }
         })
     } else {
@@ -388,14 +387,14 @@ fn handle_message_async(RawMessage { jsep, msg, txn, conn }: RawMessage) -> Mess
     }
 }
 
-extern "C" fn handle_message(handle: *mut PluginHandle, transaction: *mut c_char,
+extern "C" fn handle_message(handle: *mut PluginSession, transaction: *mut c_char,
                              message: *mut RawJanssonValue, jsep: *mut RawJanssonValue) -> *mut PluginResultInfo {
     janus::log(LogLevel::Verb, "Queueing signalling message.");
     Box::into_raw(
-        match Connection::from_ptr(handle) {
-            Ok(conn) => {
+        match Session::from_ptr(handle) {
+            Ok(sess) => {
                 let msg = RawMessage {
-                    conn: Arc::downgrade(&conn),
+                    sess: Arc::downgrade(&sess),
                     txn: transaction,
                     msg: JanssonValue::new(message),
                     jsep: JanssonValue::new(jsep)
@@ -465,7 +464,7 @@ mod tests {
         fn parse_publisher() {
             let json = r#"{"kind": "join", "role": {"kind": "publisher"}}"#;
             let result: MessageKind = serde_json::from_str(json).unwrap();
-            assert_eq!(result, MessageKind::Join { user_id: None, role: ConnectionRole::Publisher });
+            assert_eq!(result, MessageKind::Join { user_id: None, role: SessionRole::Publisher });
         }
 
         #[test]
@@ -474,7 +473,7 @@ mod tests {
             let result: MessageKind = serde_json::from_str(json).unwrap();
             assert_eq!(result, MessageKind::Join {
                 user_id: Some(UserId::try_from(1).unwrap()),
-                role: ConnectionRole::Subscriber { target_id: UserId::try_from(2).unwrap() }
+                role: SessionRole::Subscriber { target_id: UserId::try_from(2).unwrap() }
             });
         }
     }
