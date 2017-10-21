b854f8b26b4029f6bdad193cc1821159880b7eb1
Marshall Quander <marshall@quander.me>
2017-10-20 17:44:15 -0700

    Use minijanus.js in demo page

--- demo/sfu.js
+++ demo/sfu.js
@@ -1,212 +1,114 @@
-var janus = null;
-var publisher = null;
-var id = "sfu-" + Janus.randomString(12);
-var user_id = null;
+var USER_ID = null; // set on initial connection
+var ROOM_ID = 42;
 
-function getQueryParams(qs) {
-    qs = qs.split('+').join(' ');
-
-    var params = {},
-        tokens,
-        re = /[?&]?([^=]+)=([^&]*)/g;
-
-    while ((tokens = re.exec(qs))) {
-        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
-    }
-
-    return params;
-}
-
-var params = getQueryParams(document.location.search);
-var sendAudio = params.send;
+const PEER_CONNECTION_CONFIG = {
+    iceServers: [
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:global.stun.twilio.com:3478?transport=udp" }
+    ]
+};
 
 function init() {
-    // Create session
-    janus = new Janus({
-        server: "ws://localhost:8188",
-//	server: "wss://quander.me:8989",
-	// No "iceServers" is provided, meaning janus.js will use a default STUN server
-	// Here are some examples of how an iceServers field may look like to support TURN
-	// 		iceServers: [{urls: "turn:yourturnserver.com:3478", username: "janususer", credential: "januspwd"}],
-	// 		iceServers: [{urls: "turn:yourturnserver.com:443?transport=tcp", username: "janususer", credential: "januspwd"}],
-	// 		iceServers: [{urls: "turns:yourturnserver.com:443?transport=tcp", username: "janususer", credential: "januspwd"}],
-	// Should the Janus API require authentication, you can specify either the API secret or user token here too
-	//		token: "mytoken",
-	//	or
-	//		apisecret: "serversecret",
-	success: function() {
-	    // Attach to echo test plugin
-	    janus.attach({
-		plugin: "janus.plugin.sfu",
-                opaqueId: id,
-		success: function(pluginHandle) {
-		    publisher = pluginHandle;
-		    Janus.log("Plugin attached! (" + publisher.getPlugin() + ", id=" + publisher.getId() + ")");
-		    // Negotiate WebRTC
-		    Janus.debug("Trying a createOffer");
-		    publisher.createOffer({
-                        media: {audioSend: params.send !== undefined, audioRecv: false, video: false, data: true},
-			success: function(jsep) {
-			    Janus.debug("Got SDP!");
-			    Janus.debug(jsep);
-			    publisher.send({"message": {"kind": "join", "room_id": 1}, "jsep": jsep});
-			},
-			error: function(error) {
-			    Janus.error("WebRTC error:", error);
-			}
-		    });
-		},
-		error: function(error) {
-		    console.error("  -- Error attaching plugin...", error);
-		},
-		consentDialog: function(on) {
-		    Janus.debug("Consent dialog should be " + (on ? "on" : "off") + " now");
-		},
-		iceState: function(state) {
-		    Janus.log("ICE state changed to " + state);
-		},
-		mediaState: function(medium, on) {
-		    Janus.log("Janus " + (on ? "started" : "stopped") + " receiving our " + medium);
-		},
-		webrtcState: function(on) {
-		    Janus.log("Janus says our WebRTC PeerConnection is " + (on ? "up" : "down") + " now");
-		},
-		slowLink: function(uplink, nacks) {
-		    Janus.warn("Janus reports problems " + (uplink ? "sending" : "receiving") +
-			       " packets on this PeerConnection (" + nacks + " NACKs/s " + (uplink ? "received" : "sent") + ")");
-		},
-		onmessage: function(msg, jsep) {
-		    Janus.debug(" ::: Got a message :::");
-		    Janus.debug(JSON.stringify(msg));
-                    if(msg["response"] && msg.response["user_id"]) {
-                        user_id = msg.response["user_id"];
-                        var user_ids = msg.response["user_ids"];
-                        for (var i = 0; i < user_ids.length; i++) {
-                            var target_id = user_ids[i];
-                            if (user_id !== target_id) {
-                                Janus.debug("Subscribing to data for " + target_id);
-			        publisher.send({"message": {"kind": "subscribe", specs: [{"publisher_id": target_id, "content_kind": 4 /* data */}]}});
-                                Janus.debug("Creating audio subscriber connection to " + target_id);
-                                createSubscriber(user_id, target_id);
-                            }
-                        }
-                    }
-                    if(msg["event"] === "join") {
-                        var target_id = msg["user_id"];
-                        if (user_id !== target_id) {
-                            Janus.debug("Subscribing to data for " + target_id);
-			    publisher.send({"message": {"kind": "subscribe", specs:[{"publisher_id": target_id, "content_kind": 4 /* data */}]}});
-                            Janus.debug("Creating audio subscriber connection to " + target_id);
-                            createSubscriber(user_id, target_id);
-                        }
-                    }
-		    if(jsep !== undefined && jsep !== null) {
-			Janus.debug("Handling SDP as well...");
-			Janus.debug(jsep);
-			publisher.handleRemoteJsep({jsep: jsep});
-		    }
-		},
-		onlocalstream: function(stream) {
-		    Janus.debug(" ::: Got a local stream in publisher :::");
-		    Janus.debug(JSON.stringify(stream));
-		},
-		onremotestream: function(stream) {
-		    Janus.debug(" ::: Got a remote stream in publisher :::");
-		    Janus.debug(JSON.stringify(stream));
-		},
-		ondataopen: function(data) {
-		    Janus.log("The DataChannel is available!");
-		},
-		ondata: function(data) {
-		    Janus.debug("We got data from the DataChannel! " + data);
-		},
-		oncleanup: function() {
-		    Janus.log(" ::: Got a cleanup notification :::");
-		}
-	    });
-
-	},
-	error: function(error) {
-	    Janus.error(error);
-	},
-	destroyed: function() {
-	    window.location.reload();
-	}
+    var ws = new WebSocket("ws://localhost:8188", "janus-protocol");
+    ws.addEventListener("open", () => {
+        var session = new Minijanus.JanusSession(ws.send.bind(ws));
+        ws.addEventListener("message", ev => handleMessage(session, ev));
+        session.create().then(() => attachPublisher(session));
     });
 }
 
-function createSubscriber(user_id, target_id) {
-    Janus.debug("Creating subscriber for " + user_id + " to " + target_id);
-    var subscriber = null;
-    janus.attach({
-	plugin: "janus.plugin.sfu",
-        opaqueId: id,
-	success: function(pluginHandle) {
-	    subscriber = pluginHandle;
-	    Janus.log("Plugin attached! (" + subscriber.getPlugin() + ", id=" + subscriber.getId() + ")");
-	    // Negotiate WebRTC
-	    Janus.debug("Trying a createOffer");
-	    subscriber.createOffer({
-                media: {audioRecv: true, audioSend: false, video: false, data: false},
-		success: function(jsep) {
-		    Janus.debug("Got SDP!");
-		    Janus.debug(jsep);
-		    subscriber.send({"message": {"kind": "join", "room_id": 1, "user_id": user_id}, "jsep": jsep});
-                    subscriber.send({"message": {"kind": "subscribe", "specs": [{"publisher_id": target_id, "content_kind": 3 /* audio + video */}]}});
-		},
-		error: function(error) {
-		    Janus.error("WebRTC error:", error);
-		}
-	    });
-	},
-	error: function(error) {
-	    console.error("  -- Error attaching plugin...", error);
-	},
-	consentDialog: function(on) {
-	    Janus.debug("Consent dialog should be " + (on ? "on" : "off") + " now");
-	},
-	iceState: function(state) {
-	    Janus.log("ICE state changed to " + state);
-	},
-	mediaState: function(medium, on) {
-	    Janus.log("Janus " + (on ? "started" : "stopped") + " receiving our " + medium);
-	},
-	webrtcState: function(on) {
-	    Janus.log("Janus says our WebRTC PeerConnection is " + (on ? "up" : "down") + " now");
-	},
-	slowLink: function(uplink, nacks) {
-	    Janus.warn("Janus reports problems " + (uplink ? "sending" : "receiving") +
-		       " packets on this PeerConnection (" + nacks + " NACKs/s " + (uplink ? "received" : "sent") + ")");
-	},
-	onmessage: function(msg, jsep) {
-	    Janus.debug(" ::: Got a message :::");
-	    Janus.debug(JSON.stringify(msg));
-	    if(jsep !== undefined && jsep !== null) {
-		Janus.debug("Handling SDP as well...");
-		Janus.debug(jsep);
-		subscriber.handleRemoteJsep({jsep: jsep});
-	    }
-	},
-	onlocalstream: function(stream) {
-	    Janus.debug(" ::: Got a local stream in subscriber :::");
-	    Janus.debug(JSON.stringify(stream));
-	},
-	onremotestream: function(stream) {
-	    Janus.debug(" ::: Got a remote stream in subscriber :::");
-	    Janus.debug(JSON.stringify(stream));
-            var el = document.createElement("audio");
-            document.body.appendChild(el);
-            Janus.attachMediaStream(el, stream);
-            el.play();
-	},
-	ondataopen: function(data) {
-	    Janus.log("The DataChannel is available!");
-	},
-	ondata: function(data) {
-	    Janus.debug("We got data from the DataChannel! " + data);
-	},
-	oncleanup: function() {
-	    Janus.log(" ::: Got a cleanup notification :::");
-	}
+function handleMessage(session, ev) {
+    var data = JSON.parse(ev.data);
+    session.receive(data);
+    if (data.janus === "event") {
+        if (data.plugindata && data.plugindata.data) {
+            var contents = data.plugindata.data;
+            switch (contents.event) {
+            case "join":
+                if (USER_ID !== contents.user_id) {
+                    attachSubscriber(session, USER_ID, contents.user_id);
+                }
+                break;
+            case "leave":
+                // todo: tear down subscriber
+                break;
+            case undefined:
+                // a non-plugin event
+                break;
+            default:
+                console.error("Unknown event received: ", data.plugindata.data);
+                break;
+            }
+        }
+    }
+}
+
+function attachPublisher(session) {
+    console.info("Attaching publisher for session: ", session);
+    var conn = new RTCPeerConnection(PEER_CONNECTION_CONFIG);
+    var handle = new Minijanus.JanusPluginHandle(session, conn);
+    var publisher = handle.attach("janus.plugin.sfu").then(() => {
+        var iceReady = handle.negotiateIce();
+        var unreliableChannel = conn.createDataChannel("unreliable", { ordered: false, maxRetransmits: 0 });
+        var reliableChannel = conn.createDataChannel("reliable", { ordered: true });
+        var mediaReady = navigator.mediaDevices.getUserMedia({ audio: true });
+        var offerReady = mediaReady
+            .then(media => {
+                conn.addStream(media);
+                return conn.createOffer({ audio: true });
+            }, () => conn.createOffer());
+        var localReady = offerReady.then(conn.setLocalDescription.bind(conn));
+        var remoteReady = offerReady
+            .then(handle.sendJsep.bind(handle))
+            .then(answer => conn.setRemoteDescription(answer.jsep));
+        return Promise.all([iceReady, localReady, remoteReady]).then(() => {
+            console.info("Publisher connection established: ", handle);
+            return handle;
+        });
+    });
+
+    return publisher.then(handle => {
+        return handle.sendMessage({ kind: "join", room_id: ROOM_ID }).then(reply => {
+            var response = reply.plugindata.data.response;
+            USER_ID = response.user_id;
+            response.user_ids.forEach(otherId => {
+                if (USER_ID !== otherId) {
+                    attachSubscriber(session, otherId);
+                }
+            });
+        });
     });
 }
+
+function attachSubscriber(session, otherId) {
+    console.info("Attaching subscriber to " + otherId + " for session: ", session);
+    var conn = new RTCPeerConnection(PEER_CONNECTION_CONFIG);
+    var handle = new Minijanus.JanusPluginHandle(session, conn);
+    conn.addEventListener("track", function(ev) {
+        var receiverEl = document.createElement("audio");
+        document.body.appendChild(receiverEl);
+        receiverEl.srcObject = ev.streams[0];
+        receiverEl.play();
+    });
+
+    var subscriber = handle.attach("janus.plugin.sfu").then(() => {
+        var iceReady = handle.negotiateIce();
+        var offerReady = conn.createOffer({ offerToReceiveAudio: true });
+        var localReady = offerReady.then(conn.setLocalDescription.bind(conn));
+        var remoteReady = offerReady
+            .then(handle.sendJsep.bind(handle))
+            .then(answer => conn.setRemoteDescription(answer.jsep));
+        return Promise.all([iceReady, localReady, remoteReady]).then(() => {
+            console.info("Subscriber connection established: ", handle);
+            return handle;
+        });
+    });
+
+    return subscriber.then(handle => {
+        return handle.sendMessage({ kind: "join", room_id: ROOM_ID, user_id: USER_ID, subscription_specs: [
+            { content_kind: 255, publisher_id: otherId }
+        ]});
+    });
+}
+
+init();
