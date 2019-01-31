b665087d4e18e36f10c63a17f10a7f8ccec271c0
Brian Peiris <brianpeiris@gmail.com>
2019-01-30 22:14:44 -0800

    End session when kicked

--- src/lib.rs
+++ src/lib.rs
@@ -472,8 +472,13 @@ fn process_kick(from: &Arc<Session>, room_id: RoomId, user_id: UserId, token: St
     let config = STATE.config.get().unwrap();
     if let Some(ref key) = config.auth_key {
         match ValidatedToken::from_str(&token, key) {
-            Ok(_tok) => {
-                janus_info!("Processing kick from {:p} targeting user ID {} in room ID {}.", from.handle, user_id, room_id);
+            Ok(tok) => {
+                if tok.kick_users {
+                    janus_info!("Processing kick from {:p} targeting user ID {} in room ID {}.", from.handle, user_id, room_id);
+                    let end_session = gateway_callbacks().end_session;
+                    let mut switchboard = STATE.switchboard.write()?;
+                    end_session(switchboard.get_publisher(&user_id).unwrap().as_ref().as_ptr());
+                }
             }
             Err(e) => {
                 janus_warn!("Ignoring kick from {:p} due to invalid token: {}.", from.handle, e);
